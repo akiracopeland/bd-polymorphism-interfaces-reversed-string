26c253c876b63fcc4fc83017d5e78f9acf5017da
akiracopeland <akiracopeland713@gmail.com>
2024-08-17 12:20:00 -0700

    ReversedString quiz completed

--- src/com/amazon/ata/interfaces/ReversedString.java
+++ src/com/amazon/ata/interfaces/ReversedString.java
@@ -5,15 +5,39 @@ package com.amazon.ata.interfaces;
  * <p>
  * Change this class definition to implement CharSequence.
  */
-public class ReversedString {
+public class ReversedString implements CharSequence{
 
-
+    private final String reversedString;
     /**
      * Implement this constructor during the interfaces prework.
      *
      * @param chars The String of chars to be reversed
      */
     public ReversedString(final String chars) {
+        StringBuilder stringBuilder = new StringBuilder(chars);
+        this.reversedString = stringBuilder.reverse().toString();
     }
 
+    @Override
+    public int length() {
+
+        return this.reversedString.length();
+    }
+
+    @Override
+    public char charAt(int index) {
+
+        return this.reversedString.charAt(index);
+    }
+
+    @Override
+    public CharSequence subSequence(int start, int end) {
+
+        return this.reversedString.subSequence(start, end);
+    }
+
+    @Override
+    public String toString() {
+        return this.reversedString;
+    }
 }
--- tst/com/amazon/ata/interfaces/ReversedStringTest.java
+++ tst/com/amazon/ata/interfaces/ReversedStringTest.java
@@ -10,93 +10,93 @@ public class ReversedStringTest {
     @Test
     public void charAt_firstIndex_returnsFirstChar() {
         // GIVEN - "nozama"
-        CharSequence sequence = new String("amazon");
+        CharSequence sequence = new ReversedString("amazon");
 
         // WHEN - call charAt for first index
         char c = sequence.charAt(0);
 
-        // THEN - return the last character of the original String
-        assertEquals('n', c, "Expected the first index of the reversed string to be the last " +
-            "character of the original string.");
+        // THEN - return the last character of the original ReversedString
+        assertEquals('n', c, "Expected the first index of the reversed ReversedString to be the last " +
+            "character of the original ReversedString.");
     }
 
     @Test
     public void charAt_lastIndex_returnsLastChar() {
         // GIVEN - "nozama"
-        CharSequence sequence = new String("amazon");
+        CharSequence sequence = new ReversedString("amazon");
 
         // WHEN - call charAt for last index
         char c = sequence.charAt(sequence.length() - 1);
 
-        // THEN - return the first character of the original String
-        assertEquals('a', c, "Expected the last index of the reversed string to be the first " +
-            "character of the original string.");
+        // THEN - return the first character of the original ReversedString
+        assertEquals('a', c, "Expected the last index of the reversed ReversedString to be the first " +
+            "character of the original ReversedString.");
     }
 
     @Test
     public void charAt_middleIndex_returnsMiddleChar() {
         // GIVEN - "noz*ama"
-        CharSequence sequence = new String("ama*zon");
+        CharSequence sequence = new ReversedString("ama*zon");
 
         // WHEN - call charAt for middle index
         char c = sequence.charAt(3);
 
-        // THEN - middle character of the reversed & original String are the same
-        assertEquals('*', c, "Expected the middle index of the reversed string to be the same " +
-            "character from the original string.");
+        // THEN - middle character of the reversed & original ReversedString are the same
+        assertEquals('*', c, "Expected the middle index of the reversed ReversedString to be the same " +
+            "character from the original ReversedString.");
     }
 
     @Test
     public void charAt_invalidIndex_expectIndexOutOfBoundsException() {
         // GIVEN - "nozama"
-        CharSequence sequence = new String("amazon");
+        CharSequence sequence = new ReversedString("amazon");
 
         // WHEN & THEN - calling charAt with an index beyond its length results in an exception
         assertThrows(IndexOutOfBoundsException.class, () -> sequence.charAt(sequence.length()),
                      "Expected a IndexOutOfBoundsException to be thrown when accessing an invalid index of " +
-                         "the string.");
+                         "the ReversedString.");
     }
 
     @Test
-    public void length_emptyString_returnsZero() {
+    public void length_emptyReversedString_returnsZero() {
         // GIVEN - ""
-        CharSequence sequence = new String("");
+        CharSequence sequence = new ReversedString("");
 
         // WHEN - call to get the length
         int length = sequence.length();
 
-        //THEN - the empty string has length 0
-        assertEquals(0, length, "Expected an empty string to have length zero.");
+        //THEN - the empty ReversedString has length 0
+        assertEquals(0, length, "Expected an empty ReversedString to have length zero.");
     }
 
     @Test
-    public void length_nonemptyString_returnsCorrectLength() {
+    public void length_nonemptyReversedString_returnsCorrectLength() {
         // GIVEN - "nozama"
-        CharSequence sequence = new String("amazon");
+        CharSequence sequence = new ReversedString("amazon");
 
         // WHEN - call to get the length
         int length = sequence.length();
 
-        //THEN - the string has length 6
-        assertEquals(6, length, "Expected a non empty string to have non zero length.");
+        //THEN - the ReversedString has length 6
+        assertEquals(6, length, "Expected a non empty ReversedString to have non zero length.");
     }
 
     @Test
-    public void length_nonemptyString_returnsSameLengthAsNonReversedString() {
+    public void length_nonemptyReversedString_returnsSameLengthAsNonReversedReversedString() {
         // GIVEN - "nozama"
-        CharSequence sequence = new String("amazon");
+        CharSequence sequence = new ReversedString("amazon");
 
         // WHEN - call to get the length
         int length = sequence.length();
 
-        //THEN - the original string, and reversed string have the same length
-        assertEquals("amazon".length(), length, "Expected a non empty string to have non zero length.");
+        //THEN - the original ReversedString, and reversed ReversedString have the same length
+        assertEquals("amazon".length(), length, "Expected a non empty ReversedString to have non zero length.");
     }
 
     @Test
     public void subSequence_validSubSequence_returnsCorrectSubsequence() {
         // GIVEN - "nozama"
-        CharSequence sequence = new String("amazon");
+        CharSequence sequence = new ReversedString("amazon");
 
         //WHEN - ask for the first 3 letters
         CharSequence subsequence = sequence.subSequence(0, 3);
@@ -107,9 +107,9 @@ public class ReversedStringTest {
     }
 
     @Test
-    public void subSequence_entireStringAsSubSequence_returnsCorrectSubsequence() {
+    public void subSequence_entireReversedStringAsSubSequence_returnsCorrectSubsequence() {
         // GIVEN - "nozama"
-        CharSequence sequence = new String("amazon");
+        CharSequence sequence = new ReversedString("amazon");
 
         //WHEN - ask for the entire sequence
         CharSequence subsequence = sequence.subSequence(0, sequence.length());
@@ -122,47 +122,47 @@ public class ReversedStringTest {
     @Test
     public void subSequence_invalidStartIndex_throwsIndexOutOfBounds() {
         // GIVEN - "nozama"
-        CharSequence sequence = new String("amazon");
+        CharSequence sequence = new ReversedString("amazon");
 
         // WHEN & THEN - calling subsequence with an invalid start index results in an exception
         assertThrows(IndexOutOfBoundsException.class, () -> sequence.subSequence(-1, 3),
                      "Expected an IndexOutOfBoundsException to be thrown when accessing a negative index of " +
-                         "the string.");
+                         "the ReversedString.");
     }
 
     @Test
     public void subSequence_invalidEndIndex_throwsIndexOutOfBounds() {
         // GIVEN - "nozama"
-        CharSequence sequence = new String("amazon");
+        CharSequence sequence = new ReversedString("amazon");
 
         // WHEN & THEN - calling subsequence with an invalid start index results in an exception
         assertThrows(IndexOutOfBoundsException.class, () -> sequence.subSequence(0, sequence.length() + 1),
                      "Expected an IndexOutOfBoundsException to be thrown when accessing an index larger than " +
-                         "the length of the string.");
+                         "the length of the ReversedString.");
     }
 
     @Test
-    public void toString_emptyString_returnsEmptyString() {
+    public void toReversedString_emptyReversedString_returnsEmptyReversedString() {
         // GIVEN - ""
-        CharSequence sequence = new String("");
+        CharSequence sequence = new ReversedString("");
 
-        // WHEN - call toString to get the string value
-        String stringValue = sequence.toString();
+        // WHEN - call toReversedString to get the ReversedString value
+        String ReversedStringValue = sequence.toString();
 
-        // THEN - the returned string should be empty
-        assertEquals("", stringValue, "Expected toString to return an empty string.");
+        // THEN - the returned ReversedString should be empty
+        assertEquals("", ReversedStringValue, "Expected toReversedString to return an empty ReversedString.");
     }
 
     @Test
-    public void toString_nonemptyString_returnsReversedString() {
+    public void toReversedString_nonemptyReversedString_returnsReversedReversedString() {
         // GIVEN - "nozama"
-        CharSequence sequence = new String("amazon");
+        CharSequence sequence = new ReversedString("amazon");
 
-        // WHEN - call toString to get the string value
-        String stringValue = sequence.toString();
+        // WHEN - call toReversedString to get the ReversedString value
+        String ReversedStringValue = sequence.toString();
 
-        // THEN - the returned string should be the reverse of what was passed to the object
-        assertEquals("nozama", stringValue, "Expected toString to return the original string in " +
+        // THEN - the returned ReversedString should be the reverse of what was passed to the object
+        assertEquals("nozama", ReversedStringValue, "Expected toReversedString to return the original ReversedString in " +
             "reverse order.");
     }
 }
